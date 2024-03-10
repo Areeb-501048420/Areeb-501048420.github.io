637749372dff91cbfaa047d0f84fbc8756b32f3c
Areeb-501048420 <areeb.noor@torontomu.ca>
2024-03-10 18:24:49 -0400

    lab 6 submission files

---
+++ Lab 6/main.js
@@ -0,0 +1,6 @@
+
+
+
+function submit(){
+    localStorage.setItem("name","company","title","email","linkedin","hinvy","hinvn","hiringy","hiringn","hiringsummer","hiringfall","hiringwinter","hiringna","duradefault","dura4","dura8","dura12","dura16","duraO2A","duraun","durana","comments")
+}
